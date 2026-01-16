8a10573c787d294db12c0596891a9e455d4e952e
Hassan Aziz <hassanaziz0012@gmail.com>
2026-01-16 16:12:08 +0500

    debugging

---
+++ install_pydantic.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pip install -r requirements.txt && pip install --platform manylinux2014_x86_64 --target package --implementation cp --python-version 3.12 --only-binary=:all: --upgrade pydantic
